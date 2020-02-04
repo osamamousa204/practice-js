f9a4e0099f8575993ff0ebe5dd2bd9751b4974be
osamamousa204 <osamamousa204@gmail.com>
2020-02-04 21:11:57 +0200

    o

--- js/add-content.js
+++ js/add-content.js
@@ -1,13 +1,21 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-if (hourNow > 18) {
- greeting = 'Good evening, Class!';
- } else if (hourNow > 12) {
- greeting = 'Good afternoon, Class!';
- } else if (hourNow >= 0) {
- greeting = 'Good morning!';
- } else {
- greeting = 'Something went wrong!';
- }
+// var today = new Date();
+// console.log(today);
+// var hourNow = today.getHours();
+// console.log(hourNow);
+
+var hourNow = prompt("what is the time now ?");
+console.log(hourNow);
+var greeting; 
+ 
+if (hourNow > 18 && hourNow<24   ) {     
+    greeting = 'Good evening, Class!';   
+} 
+else if (hourNow > 12 && hourNow <=18) {     
+    greeting = 'Good afternoon, Class!';   
+} else if (hourNow >= 0 && hourNow <=12) {     
+    greeting = 'Good morning!';   
+} else {     
+    greeting = 'Something went wrong!';   
+} 
+
 document.write('<h3>'+greeting+'</h3>');
